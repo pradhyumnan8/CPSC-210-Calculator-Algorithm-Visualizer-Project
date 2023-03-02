9f25d60d0e3685f0bb7470e116ad52e359a4a9a2
Pradhyumnan <pvaradha@students.cs.ubc.ca>
2023-03-02 01:40:07 -0800

    Fixed few bugs in InputOutput class:

--- src/main/ui/InputOutput.java
+++ src/main/ui/InputOutput.java
@@ -26,7 +26,8 @@ public class InputOutput {
             expression = input.nextLine();
 
             if (!evaluator.isValid(expression)) {
-                System.out.println("Given expression is Invalid. Please enter a new expression:");
+                System.out.println("Given expression is Invalid");
+                System.out.println();
             } else {
                 break;
             }
@@ -48,52 +49,57 @@ public class InputOutput {
     //REQUIRES: Input must be valid
     //EFFECTS: Provides the user with a menu to continue operating the program.
     public void menu() {
-        int selection = 0;
+        String selection = "";
+        input = new Scanner(System.in);
+
         System.out.println("select from the following options:");
         System.out.println("1) view history");
         System.out.println("2) get mean of history");
         //System.out.println("3) get median of history");
         System.out.println("3) perform another calculation");
 
-        selection = input.nextInt();
+        selection = input.nextLine();
 
-        switch (selection) {
-            case 1:
-                printHistory(evaluator.getHistory());
-                break;
-            case 2:
-                System.out.println(evaluator.getHistory().mean());
-                break;
-//            case 3:
-//                System.out.println(evaluator.getHistory().median());
-//                break;
-            case 3:
-                this.calculator();
-                break;
+        if (Objects.equals(selection, "1")) {
+            System.out.println();
+            printHistory(evaluator.getHistory());
+        } else if (Objects.equals(selection, "2")) {
+            System.out.println();
+            System.out.println("The mean of your calculations is:  " + evaluator.getHistory().mean());
+        } else if (Objects.equals(selection, "3")) {
+            System.out.println();
+            this.calculator();
+        } else {
+            System.out.println();
+            System.out.printf("Invalid Input, please select again: \n\n");
+            this.menu();
         }
     }
 
     //MODIFIES: this
     //EFFECTS: main method to run all the methods for the calculator from getting input to getting input in the menu.
-    public void calculator() {
+    public int calculator() {
 
-        //expression = "";
+        input = new Scanner(System.in);
         expression = scanExpression();
         double res = evaluator.calculate(expression);
         history = evaluator.getHistory();
+        String quit;
 
-        System.out.println("The answer is:  " + res);
-        System.out.println();
+        System.out.printf("The answer is: %f\n\n", res);
 
-        System.out.println("Do you want to quit the application [y/n]?");
-        this.input = new Scanner(System.in);
-        String quit = input.next();
-
-        while (!Objects.equals(quit, "y")) {
-            this.menu();
-
+        while (true) {
             System.out.println("Do you want to quit the application [y/n]?");
-            quit = input.next();
+            quit = input.nextLine();
+
+            if (Objects.equals("y", quit)) {
+                System.exit(1);
+            } else if (Objects.equals("n", quit)) {
+                System.out.println();
+                this.menu();
+            } else {
+                System.out.println("Invalid input");
+            }
         }
     }
 }
